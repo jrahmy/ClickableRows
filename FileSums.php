209375f64218f461dd8b8f8917a803b20952df55
Jeremy P <jrahmy+github@gmail.com>
2016-03-31 20:20:08 -0700

    Update release files

--- FileSums.php
+++ FileSums.php
@@ -3,10 +3,10 @@
 /*
  * This file is part of a XenForo add-on.
  *
- * (c) Jeremy P <http://xenforo.com/community/members/jeremy-p.450/>
+ * (c) Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
  *
- * For the full copyright and license information, please view the LICENSE
- * file that was distributed with this source code.
+ * For the full copyright and license information, please view the LICENSE file
+ * that was distributed with this source code.
  */
 
 namespace Jrahmy\ClickableRows;
@@ -14,7 +14,7 @@ namespace Jrahmy\ClickableRows;
 /**
  * Filesums for XenForo File Health Check.
  *
- * @author Jeremy P <http://xenforo.com/community/members/jeremy-p.450/>
+ * @author Jeremy P <https://xenforo.com/community/members/jeremy-p.450/>
  */
 class FileSums
 {
@@ -26,8 +26,8 @@ class FileSums
     public static function getHashes()
     {
         return [
-                'library/Jrahmy/ClickableRows/Listener.php' => '9fc8291303384337334553a4ef80fbcd',
-                'js/jrahmy/clickablerows.js' => '2eb71ce154fda813b24211b53cd2135b',
+                'library/Jrahmy/ClickableRows/Listener.php' => '017692821e1968af3e6cbd3b4a91731b',
+                'js/jrahmy/clickablerows.js' => '9421a495a45c213dad40b0d311116b04',
 
         ];
     }
